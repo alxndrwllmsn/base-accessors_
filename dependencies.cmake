2f89db3cdbd9ebd0278313019840e17f30abf4eb
Max Voronkov <Maxim.Voronkov@csiro.au>
2022-08-11 07:00:21 +0800

    updated dependencies to casacore-3.5 dependent base-imagemath before merge (AXA-689)

--- dependencies.cmake
+++ dependencies.cmake
@@ -3,7 +3,5 @@ set ( LOFAR_COMMON_TAG             tags/1.2.0                               CACH
 set ( LOFAR_BLOB_TAG               tags/1.3.0                               CACHE  STRING    "lofar-blob version"               FORCE )
 set ( BASE_ASKAP_TAG               tags/1.5.0                               CACHE  STRING    "base-askap version"               FORCE )
 set ( BASE_ASKAPPARRALLEL_TAG      tags/1.3.1                               CACHE  STRING    "base-askapparrallel version"      FORCE )
-#set ( BASE_IMAGEMATH_TAG           tags/1.6.1                               CACHE  STRING    "base-imagemath version"           FORCE )
-# state after pull request #43 (casacore-3.5 changes), feature branch doesn't exist any more
-set ( BASE_IMAGEMATH_TAG           ef4aec80afe3643a9562afb4d4a8ad2451de3c0f CACHE  STRING    "base-imagemath version"           FORCE )
+set ( BASE_IMAGEMATH_TAG           tags/1.7.0                              CACHE  STRING    "base-imagemath version"           FORCE )
 set ( BASE_SCIMATH_TAG             tags/1.7.1                               CACHE  STRING    "base-scimath version"             FORCE )
